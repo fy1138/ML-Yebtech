15952e52d84a1f26c9a25e8ff82aa0530d33909b
fy1138 <133181839+fy1138@users.noreply.github.com>
2025-06-13 17:13:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('🤖 Machine Learning App')
 st.info('This is Machine Learning ')
 
 with st.expander('Data'):
-  st.write('**Raw data**')
+  st.write('**Raw data')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
